163753ed207c341e377a61e334f746ea0d433f03
James Laguardia <jameslaguardia@Jamess-MBP-2.attlocal.net>
2023-01-17 07:49:50 -0500

    progress commit

--- app/src/main/java/com/prismsoft/foody/adapters/RecipeListAdapter.kt
+++ app/src/main/java/com/prismsoft/foody/adapters/RecipeListAdapter.kt
@@ -1,29 +1,44 @@
 package com.prismsoft.foody.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.prismsoft.foody.data.model.RecipeResponse
 import com.prismsoft.foody.databinding.RecipesRowBinding
+import com.prismsoft.foody.data.model.Result
 
-class RecipeListAdapter() : RecyclerView.Adapter<RecipeListAdapter.RecipeViewHolder>() {
-
-    private val //TODO finish course
-
+class RecipeListAdapter() : ListAdapter<Result, RecipeListAdapter.RecipeViewHolder>(ResultCallback()) {
     class RecipeViewHolder(private val binding: RecipesRowBinding) : ViewHolder(binding.root) {
-        fun bind(item: Any) {
+        fun bind(item: Result) {
+            binding.result = item
+            binding.executePendingBindings()
+        }
 
+        companion object {
+            fun from(parent: ViewGroup): RecipeViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                return RecipeViewHolder(
+                    RecipesRowBinding.inflate(inflater)
+                )
+            }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder =
+        RecipeViewHolder.from(parent)
 
-    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) =
+        holder.bind(getItem(position))
+
+    fun setData(response: RecipeResponse) = submitList(response.results)
+}
+
+class ResultCallback: DiffUtil.ItemCallback<Result>() {
+    override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean =
+        oldItem.sourceName == newItem.sourceName
+
+    override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean = oldItem == newItem
 }
